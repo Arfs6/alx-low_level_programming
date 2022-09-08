65196ee3a8a98da9959f024796181fbff23816da
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-08 22:04:21 +0100

    creating 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* A file to print a string. */
+# include <stdio.h>
+/** A program to print a string. */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
