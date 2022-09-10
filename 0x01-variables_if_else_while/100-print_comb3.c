da74f7d0d82e8533fe70c96453a4e6b504465e14
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-10 19:34:34 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+/* A program to print all possible diffrent combination of two digits. */
+#include <stdio.h>
+
+/**
+ * main - initializing
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int l_num = 0;
+	int r_num = 1;
+
+	while ((l_num <= 10) && (r_num <= 9))
+	{
+		putchar(l_num % 10 + '0');
+		putchar(r_num % 10 + '0');
+putchar(',');
+		putchar(' ');
+		r_num++;
+		if (r_num == 10)
+		{
+			r_num = 0;
+			l_num++;
+			r_num = l_num;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
