12cbb0c98c378cc1c0f0b4b1339b0f111e325b5d
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-12 11:21:41 +0100

    101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,32 +11,26 @@ int main(void)
 	int num_1 = 0;
 	int num_2 = 1;
 	int num_3 = 2;
-	int i = 1;
 
 	while ((num_1 <= 10) && (num_2 <= 10) && (num_3 <= 10))
 	{
-		i++;
 		putchar(num_1 % 10 + '0');
 		putchar(num_2 % 10 + '0');
 		putchar(num_3 % 10 + '0');
 		num_3++;
-		if (num_3 >= 10)
+		if ((num_3 >= 10) && (num_2 <= 8))
 		{
 			num_2++;
-		if (num_2 == 9)
-			num_3 = 9;
-			else
 			num_3 = num_2 + 1;
 		}
-		if ((num_2 >= 10) && (num_3 >= 11))
+		if ((num_2 >= 8) && (num_3 >= 10))
 		{
-			num_1++;
-			num_2 = num_1 + 1;
-			if (num_1 == 8)
-				num_3 = 9;
-			else
+				num_1++;
+				num_2 = num_1 +1;
 				num_3 = num_2 + 1;
 		}
+		if (num_1 == 8)
+		break;
 		if (num_1 != 9)
 		{
 			putchar(',');
