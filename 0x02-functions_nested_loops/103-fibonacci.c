bc1ed09e4a75d7b5c80c77970092010231d4ba18
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-14 19:16:07 +0100

    0-main.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - initializing
+ *
+	* Return: 0
+*/
+int main(void)
+{
+	int i = 2;
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (i < 98)
+	{
+		i++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (i < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
