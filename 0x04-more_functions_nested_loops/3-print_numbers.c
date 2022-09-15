e8d7181539eaaf955e3e86d09862a13d78a1fa10
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-15 16:15:49 +0100

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,7 +7,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num ++)
-		_putchar(num + '0');
-		_putchar('\n');
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num % 10 + '0');
+	}
+	_putchar('\n');
 }
