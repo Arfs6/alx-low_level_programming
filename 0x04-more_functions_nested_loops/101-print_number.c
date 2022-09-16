30621941d1c3081cdbe0855df29f73fa5788dee5
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-16 06:07:34 +0100

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@ void print_number(int n)
 	if (n == 0)
 	{
 		_putchar('0');
-		return;
+	return;
 	}
 	else if (n < 0)
 	{
@@ -19,5 +19,4 @@ void print_number(int n)
 	if (n / 10)
 	print_number(n / 10);
 	_putchar(n % 10 + '0');
-	return;
 }
