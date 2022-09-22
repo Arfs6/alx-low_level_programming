f663487a1b0a63857d356759c8bdcf381fe585e0
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-22 04:45:11 -0400

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the array
+ * number of elements to reverse
+*/
+void reverse_array(int *a, int n)
+{
+	int cur, stop, i;
+
+	if (n % 2)
+	/* odd number, stop 1 element before the center element. */
+	stop = (n - 1) / 2;
+	else
+	/* even number, stop at the middle element. */
+	stop = n / 2;
+	i = 0; /* first element */
+	n = n - 1; /* last element */
+	while (1)
+	{
+		cur = a[n];
+		a[n] = a[i];
+		a[i] = cur;
+		i++;
+		n--;
+		if (i == stop)
+		break;
+	}
+}
