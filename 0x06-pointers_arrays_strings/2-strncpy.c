7797b630cf89e99f084a3c89c92e9f386859d196
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-21 23:51:25 -0400

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * strncpy - copy n number of char in src to dest
+ * @dest: Destination string
+ * @src: the string to be copied
+ * @n: number of char to copy
+ *
+ * Return: dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j, len;
+
+	len = 0;
+	for (i = 0; dest[i]; i++)
+	len++;
+	j = 0;
+	while (j < n)
+	{
+		if (j > n)
+		dest[j] = '\0';
+		else
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
