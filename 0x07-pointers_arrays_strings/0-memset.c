17b39b8b71f39b9d6ddc92264b48a0fccd59e437
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-26 20:34:42 +0100

    initializing 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant bite
+ * @s: memory area
+ * @b: bites
+ * @n: number of bites to fill
+ *
+ * Return: pointer to memory area
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
