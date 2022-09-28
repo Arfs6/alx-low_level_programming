b5cf3cefb2e4fabef5a71fc05d5fe5887394f976
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-09-28 21:43:33 +0100

    initializing 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _find_sqrt - find square root
+ * @n: number & int
+ * @start: starting poin
+ *
+ * Return: square root
+*/
+int _find_sqrt(int n, int start)
+{
+	if (start * start == n)
+	return (start);
+	else if (start == 2)
+	return (-1);
+	return (_find_sqrt(n, --start));
+}
+
+/**
+ * _sqrt_recursion - find square root using recursion
+ * @n: number & int
+ *
+ * Return: square root
+ * -1 if no square root
+*/
+int _sqrt_recursion(int n)
+{
+	int start, ans;
+
+	if ((n == 0) || (n == 1))
+	return (n);
+	else if (n < 0)
+	return (-1);
+
+	start = n / 2;
+	ans = _find_sqrt(n, start);
+	return (ans);
+}
