5d21af1751162861dc6b1b599a22a62f643e8cf4
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-03 18:00:57 +0100

    initializing 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to duplicated string (success)
+ * NULL if str = null
+ * NULL if unsuccessful
+*/
+char *_strdup(char *str)
+{
+	int i, len;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len]; ++len)
+	;
+
+	dup = malloc(sizeof(char) * len);
+	if (!dup)
+		return (NULL);
+
+	/* duplicate str! */
+	for (i = 0; i < len; ++i)
+		dup[i] = str[i];
+
+	return (dup);
+	}
