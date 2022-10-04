19c375d4b6bcfda9f6b5c2e06f9ef8c6b179ccc3
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-04 13:15:05 +0100

    initializing 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a two dimensional array
+ * @width: width or columns
+ * @height: height or rows
+ *
+ * Return: pointer to array
+ * 0/NULL if width or height <= 0 (two small)
+ * 0/NULL if fails (in sufficient memory)
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+	grid = (int**)malloc(sizeof(int*) * width);
+	if (!grid)
+		return (0);
+	for (i = 0; i < width; i++)
+	grid[i] = (int*)malloc(sizeof(int) * height);
+	if (!*grid)
+		return (0); /* insufficient memory */
+
+	for (i = 0; i < width; ++i)
+	{
+		j = 0;
+		for (j = 0; j < height; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
