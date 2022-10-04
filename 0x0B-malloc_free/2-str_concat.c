e94982b1eea6272ebba4321b3007d0a2b07fbf47
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-04 14:06:46 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,31 +13,28 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s1_s2;
 	int s1_len = 0, s2_len = 0, len, i, j;
-	short int flag;
 
-	flag = 1;
-	if (!s1 || !s1[0])
-		flag = 0;
-	for (s1_len = 0; s1[s1_len] && flag; ++s1_len)
+	if (!s1)
+	s1	= "";
+	for (s1_len = 0; s1[s1_len]; ++s1_len)
 		;
-	flag = 1;
-	if (!s2 || !s2[0])
-		flag = 0;
-	for (s2_len = 0; s2[s2_len] && flag; ++s2_len)
+	if (!s2)
+		s2 = "";
+	for (s2_len = 0; s2[s2_len]; ++s2_len)
 		;
 	s2_len++;
 	len = s1_len + s2_len;
-	s1_s2 = malloc(sizeof(char) * len);
+	s1_s2 = malloc(sizeof(char) * (len + 1));
 
 	if (!s1_s2)
 		return (0);
 
 	/* concactinating */
-	for (i = 0; i < s1_len; ++i)
+	for (i = 0; s1[i]; ++i)
 		s1_s2[i] = s1[i];
-	for (j = 0; i < len; ++i, ++j)
+	for (j = 0; s2[j]; ++i, ++j)
 		s1_s2[i] = s2[j];
-	s1_s2[i + 1] = '\0';
+	s1_s2[len] = '\0';
 
 	return (s1_s2);
 }
