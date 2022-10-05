6558cac184a7f84b9697c14944b8bd680b825d72
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-05 15:24:01 +0100

    initializing 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - concatenate all arguments
+ * @ac: argument count
+ * @av: argument strings
+ *
+ * Return: pointer to concatenated string
+ * NULL if fails or ac = 0
+*/
+char *argstostr(int ac, char **av)
+{
+	int len, idx, i, j;
+	char *str, *tmp;
+
+	if (!ac || !av)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i)
+	{
+		tmp = *(av + i);
+		for (j = 0; *(tmp + j); ++j)
+			len++;
+		len++; /* new line character */
+	}
+	str = malloc(sizeof(char) * (len + 1));/* + null character */
+	if (!str)
+		return (NULL); /* insufficient memory */
+
+	/* concatenating! */
+	idx = 0;
+	for (i = 0; i < ac; ++i)
+	{
+		tmp = *(av + i);
+		for (j = 0; *(tmp + j); ++j, ++idx)
+			*(str + idx) = *(tmp + j);
+		*(str + idx) = '\n';
+		++idx;
+	}
+
+	return (str);
+}
