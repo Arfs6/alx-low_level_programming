167e4168efdeb512ed47b2b18a0c3d33b9a20b9b
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-10 22:18:21 +0100

    initializing 2-my_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (!d)
 		return; /* dog is null */
 
-	d->name ? printf("Name: %s\n", d->name) : printf("Name: (nill)\n");
-	d->age ? printf("Age: %.6f\n", d->age) : printf("Age: (nill)\n");
-	d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nill)\n");
+	d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
+	d->age ? printf("Age: %.6f\n", d->age) : printf("Age: (nil)\n");
+	d->owner ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 }
