c62af72c2f840137a0749836d86e774ed21df167
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-20 18:46:46 +0100

    initializing 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print the contents of a list_t link list
+ * @h: pointer to first element
+ *
+ * Return: number of elements printed
+*/
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	if (!h)
+		return (0);
+
+	while(1)
+	{
+		if (h->str)
+			printf("[%i] %s\n", h->len, h->str);
+		else
+			printf("[0] (nill)\n");
+		i++;
+		if (!h->next)
+			break;
+		h = h->next;
+	}
+
+	return (i);
+}
