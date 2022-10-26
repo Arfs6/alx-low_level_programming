bc9cded4d8818c1f3caf29a3304daf8e9f8015ce
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-26 21:12:44 +0100

    maybe i'm done'

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,10 +7,11 @@
 *n: int part of listint_t
 *
 * Return: pointer to new node
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+*/
+listint_t *add_nodeint_end(listint_t *(*head), const int n)
 {
 	listint_t *new;
+	listint_t *temp;
 
 	if (head == NULL)
 		return (NULL);
@@ -22,17 +23,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	while (*head->next && *head)
-		*head = *head->next;
-
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
-	else
-	{
-		*head->next = new;
-	}
+
+	temp = *head;
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
 
 	return (new);
 }
