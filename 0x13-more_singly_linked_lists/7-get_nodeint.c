d0bbca1ce773c7d5222e25b2b82f6987fed54b8e
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-10-30 14:46:51 +0100

    done with task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,35 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * tmp - check the code
+ * get_nodeint_at_index - get node at passed index
+ * @head: pointer to first node
+ * @index: index to node that will be returned
  *
- * Return: always 0
+ * Return: pointer to node
+ * NULL if fails
  */
-int tmp(void)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	return (0);
+	listint_t *node;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	node = head;
+	while (i <= index)
+	{
+		if (i == index)
+			return (node);
+		else if (node->next != NULL)
+		{
+			node = node->next;
+			i++;
+		}
+		else
+			return (NULL);
+	}
+
+	return (NULL);
 }
