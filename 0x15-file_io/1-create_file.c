08598f1658bc452ed60406f93e2df385df746c92
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-11-01 15:02:55 +0100

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	for (len = 0; text_content[len] && text_content; len++)
+	for (len = 0; text_content && text_content[len]; len++)
 		;
 
 	fn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
