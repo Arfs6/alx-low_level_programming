273ca625f94557001187d772a30249dcd6887ac0
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-12-08 20:07:47 +0100

    bug fixes

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	else if (*h == NULL && idx == 0)
+	else if (idx == 0)
 		return (add_dnodeint(h, n));
 	else if (*h == NULL && idx > 0)
 		return (NULL);
