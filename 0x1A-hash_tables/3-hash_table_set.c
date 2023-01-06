140242565c8c00b26947fec42f305ceeb7e22b95
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-01-06 09:35:56 +0100

    now checking for ht->array is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *newNode;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL || ht->array == NULL)
 		return (0);
 	else if (*key == '\0')
 		return (0);
