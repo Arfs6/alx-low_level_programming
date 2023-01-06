02bd5de86ceba81630bac68ebb3bce39a701764d
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-01-06 09:30:11 +0100

    fixed weeror

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else if (*key == '\0')
 		return (0);
 
-	idx = key_index(key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
 
 	/* create the node and add it's key and value */
 	newNode = malloc(sizeof(*newNode));
