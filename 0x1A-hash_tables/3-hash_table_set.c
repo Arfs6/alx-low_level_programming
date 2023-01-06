805472f362ca53e3618501e29434cd4e0c6b64b0
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-01-06 19:18:45 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+void add_node(hash_node_t *node, hasht_table_t *ht);
+
 /**
  * hash_table_set - add a key value pair to the hash table
  * @ht: hash table
@@ -16,19 +18,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *newNode, *node;
 
-	/* check if parameters are valid */
-	if (key == NULL || value == NULL || ht == NULL || ht->array == NULL)
-		return (0);
-	else if (*key == '\0' || ht->size == 0)
-		return (0);
+	if (!key || !*key || !value || !ht || !(ht->array))
+	return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-
-	/* create the node and add it's key and value */
 	newNode = malloc(sizeof(*newNode));
 	if (newNode == NULL)
 		return (0);
-
 	newNode->key = strdup(key);
 	if (newNode->key == NULL)
 	{
@@ -46,11 +42,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/* add the node to the begining of the list at it's index */
 	if (ht->array[idx] == NULL)
 	{
-		/* first item */
 		ht->array[idx] = newNode;
 		return (1);
 	}
 
+	add_node(node, ht);
+
+	return (1);
+}
+
+/**
+ * add_node - add the node to the array
+ * @node: node to add to the link list.
+ * @ht: hash table pointer.
+ */
+void add_node(hash_node_t *node, hasht_table_t *ht)
+{
 	node = ht->array[idx];
 	while (node)
 	{
@@ -62,9 +69,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-
-		newNode->next = ht->array[idx];
-		ht->array[idx] = newNode;
-
+	newNode->next = ht->array[idx];
+	ht->array[idx] = newNode;
 	return (1);
 }
